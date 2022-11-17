7abaa7428ca7d490421439052f26cf458409ec62
Gabriele <gabrieletuozzo@gmail.com>
2022-11-17 18:44:54 +0100

    fixed bug in tweet filter

--- TweepyTest.py
+++ TweepyTest.py
@@ -21,17 +21,18 @@ def getMatchTw(matchAbbr,filter):
     client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAOTujAEAAAAAyX1EGWg%2FgxFP%2Bvtq4MD4Ro%2BhvhI%3DwcWzJBmMeTJbzuXDHguk8yQY6miHUeIzQ211suzm6Jci7L5LwO')
     query = '#%s -is:retweet lang:en'%matchAbbr
     tweets = tweepy.Paginator(client.search_recent_tweets,query, tweet_fields=['context_annotations', 'created_at'],max_results=100).flatten(limit=10)
-    tweetsLi = list(tweets)
+    filteredTweets = []
     if filter == False: #da chiamare con false il giorno del pre-partita (?)
-        return tweetsLi
+        return tweets
     else:
-        for tweet in tweetsLi:
-            if filter > tweet.created_at: #per filtrare i tweet fatti il giorno dopo della partita
-                tweetsLi.remove(tweet)
-        return tweetsLi
-'''        
-filter = datetime.datetime(2022,11,17,12,00,00,tzinfo=pytz.utc)
+        for tweet in tweets:
+            if filter < tweet.created_at: #per filtrare i tweet fatti il giorno dopo della partita
+                filteredTweets.append(tweet)
+        return filteredTweets
+
+'''
+filter = datetime.datetime(2022,11,17,00,00,00,tzinfo=pytz.utc)
 tweets = getMatchTw('QATECU',filter)
 for tweet in tweets:
-    print(tweet.created_at) 
+    print(tweet.created_at)
 '''
