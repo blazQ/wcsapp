397bdff1e6acc1be06dcc2189070e4e8a27173cf
Gabriele <gabrieletuozzo@gmail.com>
2022-11-27 10:38:55 +0100

    aggiornamento dataset con partite del 26 Novembre

--- tweet_retrieval_b.bat
+++ tweet_retrieval_b.bat
@@ -1,2 +1,2 @@
-@py.exe .\tweet_retrieval.py --Match=BRASRB --Keywords=Brazil  --Start=11-24T22:15 --End=11-26T08:00
+@py.exe .\tweet_retrieval.py --Match=ARGMEX --Keywords=Argentina --Start=11-26T22:00 --End=11-27T08:30
 @pause
