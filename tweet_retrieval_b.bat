529b94fdc430e6bc10ba1005cf493bda31ed8902
Pietro <p.negri4@studenti.unisa.it>
2022-11-30 14:53:02 +0100

    Dati relativi al 25, 27 e parzialmente 29 Novembbre

--- tweet_retrieval_b.bat
+++ tweet_retrieval_b.bat
@@ -1,2 +1,2 @@
-@py.exe .\tweet_retrieval.py --Match=ARGMEX --Keywords=Argentina --Start=11-26T22:00 --End=11-27T08:30
+@py.exe .\tweet_retrieval.py --Match=IRNUSA --Keywords=Iran,Usa --Start=11-29T19:00 --End=11-29T20:00
 @pause
