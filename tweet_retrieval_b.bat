4292480dd973743ee14f25d82d01e0493239510c
Gabriele <gabrieletuozzo@gmail.com>
2022-12-01 17:53:35 +0100

    risultati 30 Novembre

--- tweet_retrieval_b.bat
+++ tweet_retrieval_b.bat
@@ -1,2 +1,2 @@
-@py.exe .\tweet_retrieval.py --Match=PORURU --Keywords=Portugal,Uruguay --Start=11-28T12:00 --End=11-28T21:00
+@py.exe .\tweet_retrieval.py --Match=TUNFRA --Keywords=Tunisia,France --Start=11-30T00:00 --End=11-30T14:59
 @pause
