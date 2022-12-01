5dcaf7fba78cafdb76f12c0a3641eb154b55e81e
Gabriele <gabrieletuozzo@gmail.com>
2022-12-01 17:55:29 +0100

    partite del 30 Novembre

--- tweet_predictions_retrieval_b.bat
+++ tweet_predictions_retrieval_b.bat
@@ -1,2 +1,2 @@
-@py.exe .\tweet_predictions_retrieval.py --National=Portugal --Start=11-20T00:00 --End=11-23T16:30
+@py.exe .\tweet_predictions_retrieval.py --National=Iran --Start=11-20T00:00 --End=11-23T16:30
 @pause
