2372feab6bbfce6887659249f6672ee2c573ecd9
Gabriele <gabrieletuozzo@gmail.com>
2022-12-06 12:02:16 +0100

    Inseriti dati delle partite del 5 Dicembre

--- tweet_retrieval_b.bat
+++ tweet_retrieval_b.bat
@@ -1,2 +1,2 @@
-@py.exe .\tweet_retrieval.py --Match=ARGAUS --Keywords=Argentina,Australia --Start=12-03T00:00 --End=12-03T20:30
+@py.exe .\tweet_retrieval.py --Match=BRAKOR --Keywords=Brazil,Korea --Start=12-05T00:00 --End=12-05T21:30
 @pause0
